8d5a279d51192d150730bc82f55b6d54871c1337
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-26 22:55:21 +0100

    Added postsave signal to create the user profile when an account is created

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
@@ -20,3 +22,13 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+        instance.userprofile.save()
