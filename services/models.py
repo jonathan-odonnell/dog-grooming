14f5099c8bc5b4a06ff51b7db576c6edac1ce23d
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-27 00:52:15 +0100

    Added service model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    offer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
