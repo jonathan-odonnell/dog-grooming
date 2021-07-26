77ff3163eca7acf27db177b9ab1ccd66f4452f0e
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-26 22:32:39 +0100

    Added profiles app

--- dog_groming/settings.py
+++ dog_groming/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount'
+    'allauth.socialaccount',
+    'profiles'
 ]
 
 MIDDLEWARE = [
