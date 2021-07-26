17ab7009e7363d9e71abf0cd07968bf0e2389bb4
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-27 00:36:19 +0100

    Added profile view, url and template

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm
 
-# Create your views here.
+
+@login_required
+def profile(request):
+    """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            profile = form.save()
+            profile.user.email = request.POST['email_address']
+            profile.user.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(
+                request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
