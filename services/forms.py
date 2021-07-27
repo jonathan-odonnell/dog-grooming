6792366775e36abf5a13911776148532ab4be987
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-28 00:15:16 +0100

    Amended placeholders in service form

--- services/forms.py
+++ services/forms.py
@@ -19,6 +19,7 @@ class ServiceForm(forms.ModelForm):
         placeholders = {
             'name': 'Name',
             'price': 'Price',
+            'size': 'Size',
             'description': 'Description',
         }
 
