a062f5c7bb9ca773277512c3b8a61aa5d6895857
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-28 00:25:31 +0100

    Added total before tax and tax amounts to context processor

--- bag/context.py
+++ bag/context.py
@@ -1,16 +1,17 @@
 from django.shortcuts import get_object_or_404
 from services.models import Service
+from decimal import Decimal
 
 
 def bag_contents(request):
     services = []
-    total = 0
+    grand_total = 0
     item_count = 0
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
         service = get_object_or_404(Service, id=item_id)
-        total += item_data * service.price
+        grand_total += item_data * service.price
         item_count += item_data
         services.append({
             'item_id': item_id,
@@ -18,9 +19,14 @@ def bag_contents(request):
             'service': service,
         })
 
+    tax = Decimal(grand_total * 0.2)
+    total = Decimal(grand_total - tax)
+
     context = {
         'services': services,
         'total': total,
+        'tax': tax,
+        'grand_total': grand_total,
         'item_count': item_count,
     }
 
