23e4a86425bc341d65e6dc733dbc8ba90b3c9d7f
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-28 00:13:46 +0100

    Added size field to service model

--- services/models.py
+++ services/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Service(models.Model):
     name = models.CharField(max_length=254)
-    description = models.TextField()
+    size = models.CharField(max_length=2)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
     offer = models.BooleanField(default=False)
 
     def __str__(self):
