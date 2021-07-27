dce9e15655fe9ace1a9d7a5489f0e08688619db1
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-27 22:52:51 +0100

    Added stripe settings to settings.py

--- dog_groming/settings.py
+++ dog_groming/settings.py
@@ -159,3 +159,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# Stripe
+
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
