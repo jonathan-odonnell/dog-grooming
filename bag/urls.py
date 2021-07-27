c263913dd02691731231f5e90cc14931dbe085d1
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-27 16:57:00 +0100

    Added add service and remove service views and urls

--- bag/urls.py
+++ bag/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.BagView.as_view(), name='bag'),
+    path('add/service/<int:item_id>/', views.AddServiceView.as_view(), name='add_service_to_bag'),
+    path('remove/service/<item_id>/', views.RemoveServiceView.as_view(), name='remove_service_from_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,43 @@
 from django.views.generic.base import View, TemplateView
+from django.shortcuts import redirect, get_object_or_404, HttpResponse
+from django.contrib import messages
+from services.models import Service
 
 
 class BagView(TemplateView):
     template_name = "bag/bag.html"
+
+
+class AddServiceView(View):
+    """ Add the specified service to the shopping bag """
+    def post(self, request, item_id):
+        service = get_object_or_404(Service, id=item_id)
+        redirect_url = request.POST.get('redirect_url')
+        bag = request.session.get('bag', {})
+
+        if item_id in list(bag['services'].keys()):
+            bag['services'][item_id] += 1
+            messages.success(
+                request, f'Added {service.name} to bag')
+        else:
+            bag['services'][item_id] = 1
+            messages.success(request, f'Added {service.name} to bag')
+
+        request.session['bag'] = bag
+        return redirect(redirect_url)
+
+
+class RemoveServiceView(View):
+    """ Removes the specified service from the shopping bag """
+    def post(self, request, item_id):
+        try:
+            service = get_object_or_404(Service, id=item_id)
+            bag = request.session.get('bag', {})
+            bag['services'].pop(item_id)
+            messages.success(request, f'Removed {service.name} from your bag')
+            request.session['bag'] = bag
+            return HttpResponse(status=200)
+
+        except Exception as e:
+            messages.error(request, f'Error removing item: {e}')
+            return HttpResponse(status=500)
