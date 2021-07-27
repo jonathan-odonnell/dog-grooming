2dbba61f1e48de4a07507e26050882c95288b8b3
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-27 14:49:40 +0100

    Changed profile view to class based view

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile'),
+    path('', views.ProfileView.as_view(), name='profile'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,31 +1,36 @@
-from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
+from django.shortcuts import render
+from django.views import View
+from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
 
-@login_required
-def profile(request):
+class ProfileView(View):
     """ Display the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
 
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
+    model = UserProfile
+    form_class = UserProfileForm
+    template_name = 'profiles/profile.html'
+
+    @method_decorator(login_required)
+    def get(self, request):
+        form = self.form_class(initial={'email_address': request.user.email})
+        return render(request, self.template_name, {'form': form})
+
+    @method_decorator(login_required)
+    def post(self, request):
+        form = self.form_class(request.POST)
+
         if form.is_valid():
             profile = form.save()
             profile.user.email = request.POST['email_address']
             profile.user.save()
             messages.success(request, 'Profile updated successfully')
+
         else:
             messages.error(
                 request, 'Update failed. Please ensure the form is valid.')
-    else:
-        form = UserProfileForm(instance=profile)
 
-    template = 'profiles/profile.html'
-    context = {
-        'form': form,
-    }
-
-    return render(request, template, context)
+        return render(request, self.template_name, {'form': form})
