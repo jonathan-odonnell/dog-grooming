ac0663711c6b301f2bc8b8782de2a8043429337e
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-28 01:35:29 +0100

    Added messages to services views

--- services/views.py
+++ services/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Service
@@ -23,6 +24,16 @@ class AddServiceView(LoginRequiredMixin, SuperUserRequired, CreateView):
     template_name = 'services/add_service.html'
     success_url = reverse_lazy('services')
 
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            messages.success(request, 'Successfully added service!')
+        else:
+            messages.error(
+                request, 'Failed to add service. \
+                    Please ensure the form is valid.')
+        return super().post(request, *args, **kwargs)
+
 
 class EditServiceView(LoginRequiredMixin, SuperUserRequired, UpdateView):
     model = Service
@@ -30,7 +41,20 @@ class EditServiceView(LoginRequiredMixin, SuperUserRequired, UpdateView):
     template_name = 'services/edit_service.html'
     success_url = reverse_lazy('services')
 
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            messages.success(request, 'Successfully updated service!')
+        else:
+            messages.error(request, 'Failed to update service. \
+                Please ensure the form is valid.')
+        return super().post(request, *args, **kwargs)
+
 
 class DeleteServiceView(LoginRequiredMixin, SuperUserRequired, DeleteView):
     model = Service
     success_url = reverse_lazy('services')
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, 'Successfully deleted service!')
+        return super().post(request, *args, **kwargs)
