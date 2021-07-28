b2d95fe17316c7c66198704a5a77c252b05e7cba
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-28 18:03:54 +0100

    Added webhooks url

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import WebhookView
 
 urlpatterns = [
     path('', views.CheckoutView.as_view(), name='checkout'),
     path('success/', views.CheckoutSuccessView.as_view(), name='checkout_success'),
     path('cache-checkout-data/', views.CacheCheckoutView.as_view(), name='cache_checkout_data'),
+    path('wh/', WebhookView.as_view(), name='webhook')
 ]
