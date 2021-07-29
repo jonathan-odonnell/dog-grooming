e31ae999090dfb30f92143d69d289b963174306d
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-29 14:36:47 +0100

    renamed email address field to email in contact model

--- contact/models.py
+++ contact/models.py
@@ -8,7 +8,7 @@ class Contact(models.Model):
         ordering = ('-date',)
 
     full_name = models.CharField(max_length=50)
-    email_address = models.EmailField(max_length=50)
+    email = models.EmailField(max_length=50)
     user_profile = models.OneToOneField(UserProfile, on_delete=models.SET_NULL,
                                         null=True, blank=True,
                                         related_name='contact')
