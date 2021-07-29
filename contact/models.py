28059276205b2ccc456f9da8bdfd7ac7822a2db7
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-29 14:32:05 +0100

    Added contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from profiles.models import UserProfile
 
-# Create your models here.
+
+class Contact(models.Model):
+
+    class Meta:
+        ordering = ('-date',)
+
+    full_name = models.CharField(max_length=50)
+    email_address = models.EmailField(max_length=50)
+    user_profile = models.OneToOneField(UserProfile, on_delete=models.SET_NULL,
+                                        null=True, blank=True,
+                                        related_name='contact')
+    date = models.DateTimeField(auto_now=True)
+    subject = models.CharField(max_length=50)
+    message = models.TextField()
+
+    def __str__(self):
+        return f'{self.full_name} - {self.subject}'
