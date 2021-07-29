b09f09ab6846cfcb93e55bf4456b233ee55ce262
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-29 14:52:03 +0100

    Added contact url, view and template

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Contact
+from .forms import ContactForm
 
-# Create your views here.
+
+class ContactView(CreateView):
+    model = Contact
+    form_class = ContactForm
+    template_name = 'contact/contact.html'
+    success_url = reverse_lazy('contact')
+
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:
+            contact = form.save(commit=False)
+            contact.user_profile = self.user.userprofile
+            contact.save()
+        else:
+            form.save()
+        return self.get_success_url()
