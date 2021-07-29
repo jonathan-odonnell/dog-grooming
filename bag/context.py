717d9e7969dabed2ef1a78620ece8fde2a708411
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-29 15:44:29 +0100

    Added coupons and amended total calculations in context processor

--- bag/context.py
+++ bag/context.py
@@ -1,17 +1,21 @@
 from django.shortcuts import get_object_or_404
 from services.models import Service
 from decimal import Decimal
+from datetime import date
 
 
 def bag_contents(request):
     services = []
+    order_total = 0
     grand_total = 0
+    discount = 0
     item_count = 0
     bag = request.session.get('bag', {})
+    coupon = request.session.get('coupon', '')
 
     for item_id, item_quantity in bag.items():
         service = get_object_or_404(Service, id=item_id)
-        grand_total += item_quantity * service.price
+        order_total += item_quantity * service.price
         item_count += item_quantity
         services.append({
             'item_id': item_id,
@@ -19,12 +23,23 @@ def bag_contents(request):
             'service': service,
         })
 
-    tax = Decimal(grand_total * 0.2)
-    total = Decimal(grand_total - tax)
+    if coupon:
+        current_date = date.today()
+        coupon_qs = get_object_or_404(
+            name=coupon,
+            start_date_gte=current_date,
+            end_date_lte=current_date)
+        discount = coupon_qs.amount
+
+    order_total = Decimal(order_total * 0.8)
+    tax = Decimal(order_total * 0.2)
+    grand_total = Decimal(order_total + tax - discount)
 
     context = {
         'services': services,
-        'total': total,
+        'order_total': order_total,
+        'coupon': coupon,
+        'discount': discount,
         'tax': tax,
         'grand_total': grand_total,
         'item_count': item_count,
