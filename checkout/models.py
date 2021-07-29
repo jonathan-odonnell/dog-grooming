efb477966d4ea6b3fcd7bada9c28a6cafb646aed
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-29 14:57:17 +0100

    Added coupon model

--- checkout/models.py
+++ checkout/models.py
@@ -72,3 +72,11 @@ class OrderLineItem(models.Model):
     def __str__(self):
         return f'{self.service.name} on order {self.order.order_number}'
 
+
+class Coupon(models.Model):
+    name = models.CharField(max_length=20)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.name} {self.start_date} - {self.end_date}'
