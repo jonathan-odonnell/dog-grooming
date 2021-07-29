ef8ea00df49c1e58b311a5ffbf72e0ed3cd55a09
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-29 16:34:59 +0100

    Added contact admin view

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('date',)
+
+    fields = ('full_name', 'email', 'user_profile',
+              'date', 'subject', 'message',)
+
+    list_display = ('full_name', 'email', 'subject', 'date',)
+
+
+admin.site.register(Contact, ContactAdmin)
