93e023ef6aef6a4afd01707f6a7a3a18d25f2cb0
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-30 18:18:54 +0100

    Fixed an issue with the post request redirect url in the checkout view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .models import Contact
@@ -10,11 +11,14 @@ class ContactView(CreateView):
     template_name = 'contact/contact.html'
     success_url = reverse_lazy('contact')
 
+    def get_success_url(self):
+        return self.success_url
+
     def form_valid(self, form):
         if self.request.user.is_authenticated:
             contact = form.save(commit=False)
-            contact.user_profile = self.user.userprofile
+            contact.user_profile = self.request.user.userprofile
             contact.save()
         else:
             form.save()
-        return self.get_success_url()
+        return redirect(self.get_success_url())
