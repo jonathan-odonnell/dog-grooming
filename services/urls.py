3abe8576df166ccaae43f631b850ad6b1e8a4410
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-04 21:34:43 +0100

    Added service appointments by month url

--- services/urls.py
+++ services/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.ServicesView.as_view(), name='services'),
     path('<int:pk>/appointments/', views.AppointmentsView.as_view(), name='service_appointments'),
+    path('<int:pk>/appointments/<int:month>/<int:year>/', views.AppointmentsView.as_view(), name='service_appointments_by_month'),
     path('add/', views.AddServiceView.as_view(), name='add_service'),
     path('edit/<int:pk>/', views.EditServiceView.as_view(), name='edit_service'),
     path('delete/<int:pk>/', views.DeleteServiceView.as_view(),
