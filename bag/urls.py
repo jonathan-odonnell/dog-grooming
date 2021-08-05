8022a5a49b679a4d521055110f5be72050f3d600
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-06 00:49:48 +0100

    Amended add service url

--- bag/urls.py
+++ bag/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.BagView.as_view(), name='bag'),
-    path('add/service/<int:item_id>/',
+    path('add/service/<item_id>/',
          views.AddServiceToBagView.as_view(), name='add_service_to_bag'),
     path('remove/service/<item_id>/', views.RemoveServiceFromBagView.as_view(),
          name='remove_service_from_bag'),
