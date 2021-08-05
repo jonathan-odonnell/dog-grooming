5639c5bf3472624411baf97bcf21f8ba6bd3c79c
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-05 21:33:54 +0100

    Refactored code in the orders and order details views

--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import UpdateView
-from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.conf import settings
@@ -44,27 +44,26 @@ class ProfileView(LoginRequiredMixin, UpdateView):
         return self.render_to_response(self.get_context_data(form=form))
 
 
-class OrdersView(LoginRequiredMixin, TemplateView):
+class OrdersView(LoginRequiredMixin, ListView):
     """ Display the user's orders. """
+    context_object_name = 'orders'
     template_name = 'profiles/orders.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['orders'] = Order.objects.filter(
+    def get_queryset(self):
+        return Order.objects.filter(
             user_profile=self.request.user.userprofile)
-        return context
 
 
-class OrderDetailsView(LoginRequiredMixin, TemplateView):
+class OrderDetailsView(LoginRequiredMixin, DetailView):
     """ Display the user's orders. """
+    context_object_name = 'order'
     template_name = 'checkout/checkout_success.html'
 
-    def get(self, request, order_number, *args, **kwargs):
-        self.order = get_object_or_404(Order, order_number=order_number)
-        return super().get(self, request, order_number, *args, **kwargs)
+    def get_object(self):
+        return get_object_or_404(
+            Order, order_number=self.kwargs['order_number'])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['order'] = self.order
         context['from_orders'] = True
         return context
