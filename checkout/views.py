413588f923f0a4c1a8acd51ccb3497de7ed078a3
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-06 00:42:09 +0100

    Refactored code in checkout and checkout success views

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import (
-    render, redirect, reverse, get_object_or_404, HttpResponse)
+    redirect, reverse, get_object_or_404, HttpResponse)
 from django.contrib import messages
 from django.views import View
-from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.conf import settings
 from django.utils.timezone import make_aware
@@ -36,7 +37,8 @@ class CacheCheckoutView(View):
             return HttpResponse(content=e, status=400)
 
 
-class CheckoutView(LoginRequiredMixin, View):
+class CheckoutView(LoginRequiredMixin, CreateView):
+    model = Order
     form_class = OrderForm
     template_name = 'checkout/checkout.html'
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -54,14 +56,31 @@ class CheckoutView(LoginRequiredMixin, View):
         )
         return intent
 
-    def get_context_data(self, form):
-        context = {
-            'form': form,
-            'stripe_public_key': self.stripe_public_key,
-            'client_secret': self.intent.client_secret,
-            'google_api_key': self.google_api_key,
+    def get_initial(self):
+        profile = UserProfile.objects.get(user=self.request.user)
+        initial = {
+            'full_name': profile.user.get_full_name(),
+            'email': profile.user.email,
+            'phone_number': profile.phone_number,
+            'address_line_1': profile.address_line_1,
+            'address_line_2': profile.address_line_2,
+            'town_or_city': profile.town_or_city,
+            'county': profile.county,
+            'country': profile.country,
+            'postcode': profile.postcode,
         }
+        return initial
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['stripe_public_key'] = self.stripe_public_key
+        context['client_secret'] = self.intent.client_secret
+        context['google_api_key'] = self.google_api_key
         return context
+
+    def get_success_url(self):
+        return redirect(reverse(
+                'checkout_success', args=[self.object.order_number]))
 
     def get(self, request):
         bag = request.session.get('bag', {})
@@ -70,26 +89,10 @@ class CheckoutView(LoginRequiredMixin, View):
                 request, "There's nothing in your bag at the moment")
             return redirect(reverse('services'))
 
+        self.object = None
         self.intent = self.create_intent(request)
-
-        try:
-            profile = UserProfile.objects.get(user=request.user)
-            form = self.form_class(initial={
-                'full_name': profile.user.get_full_name(),
-                'email': profile.user.email,
-                'phone_number': profile.phone_number,
-                'address_line_1': profile.address_line_1,
-                'address_line_2': profile.address_line_2,
-                'town_or_city': profile.town_or_city,
-                'county': profile.county,
-                'country': profile.country,
-                'postcode': profile.postcode,
-            })
-        except UserProfile.DoesNotExist:
-            form = self.form_class()
-
-        context = self.get_context_data(form)
-        return render(request, self.template_name, context)
+        self.initial = self.get_initial()
+        return self.render_to_response(self.get_context_data())
 
     def post(self, request):
         bag = request.session.get('bag', {})
@@ -107,59 +110,52 @@ class CheckoutView(LoginRequiredMixin, View):
                 end_date_lte=current_date
             )
 
-        form_data = {
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'phone_number': request.POST['phone_number'],
-            'address_line_1': request.POST['address_line_1'],
-            'address_line_2': request.POST['address_line_2'],
-            'town_or_city': request.POST['town_or_city'],
-            'county': request.POST['county'],
-            'country': request.POST['country'],
-            'postcode': request.POST['postcode'],
-        }
-
-        form = OrderForm(form_data)
+        form = self.get_form()
         if form.is_valid():
-            order = form.save(commit=False)
-            order.coupon = coupon_qs
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.user_profile = profile
-            order.save()
-
-            for item_id, item_data in bag['services'].items():
-                try:
-                    service = Service.objects.get(id=item_id)
-                    OrderLineItem.objects.create(
-                        order=order,
-                        service=service,
-                        quantity=item_data['quantity'],
-                        size=service.size,
-                    )
-                    for appointment in item_data['appointments']:
-                        start_time = make_aware(datetime.strptime(
-                            appointment, '%d/%m/%Y %H:%M'))
-                        end_time = start_time + timedelta(hours=2)
-                        Appointment.objects.create(
-                            order=order,
-                            start=start_time,
-                            end=end_time
-                        )
-                except Service.DoesNotExist:
-                    messages.error(request, (
-                        "One of the services in your bag wasn't found in \
-                            our database. Please call us for assistance!")
-                    )
-                    order.delete()
-                    return redirect(reverse('bag'))
-            return redirect(reverse(
-                'checkout_success', args=[order.order_number]))
+            return self.form_valid(form, profile, coupon_qs, bag)
         else:
-            messages.error(request, 'There was an error with your form. \
+            return self.form_invalid(form)
+
+    def form_valid(self, form, profile, coupon, bag):
+        self.object = form.save(commit=False)
+        self.object.coupon = coupon
+        pid = self.request.POST.get('client_secret').split('_secret')[0]
+        self.object.stripe_pid = pid
+        self.object.user_profile = profile
+        self.object.save()
+
+        for item_id, item_data in bag['services'].items():
+            try:
+                service = Service.objects.get(id=item_id)
+                OrderLineItem.objects.create(
+                    order=self.object,
+                    service=service,
+                    quantity=item_data['quantity'],
+                    size=service.size,
+                )
+                for appointment in item_data['appointments']:
+                    start_time = make_aware(datetime.strptime(
+                        appointment, '%d/%m/%Y %H:%M'))
+                    end_time = start_time + timedelta(hours=2)
+                    Appointment.objects.create(
+                        order=self.object,
+                        start=start_time,
+                        end=end_time
+                    )
+            except Service.DoesNotExist:
+                messages.error(self.request, (
+                    "One of the services in your bag wasn't found in \
+                        our database. Please call us for assistance!")
+                )
+                self.object.delete()
+                return redirect(reverse('bag'))
+
+        return self.get_success_url()
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'There was an error with your form. \
                 Please double check your information.')
-            context = self.get_context_data(form)
-            return render(request, self.template_name, context)
+        return self.render_to_response(self.get_context_data(form=form))
 
 
 class AddCouponView(View):
@@ -177,18 +173,17 @@ class AddCouponView(View):
             return HttpResponse(status=500)
 
 
-class CheckoutSuccessView(LoginRequiredMixin, TemplateView):
+class CheckoutSuccessView(LoginRequiredMixin, DetailView):
     """ Handle successful checkouts """
-
+    context_object_name = 'order'
     template_name = "checkout/checkout_success.html"
 
+    def get_object(self):
+        return get_object_or_404(
+            Order, order_number=self.kwargs['order_number'])
+
     def get(self, request, order_number, *args, **kwargs):
-        self.order = get_object_or_404(Order, order_number=order_number)
+        self.object = self.get_object()
         if 'bag' in request.session:
             del request.session['bag']
-        return super().get(self, request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['order'] = self.order
-        return context
+        return self.render_to_response(self.get_context_data())
