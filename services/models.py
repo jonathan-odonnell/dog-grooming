2c2f9d63161765433085238ef813aa36d5617080
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-06 16:41:08 +0100

    Added image field to service model

--- services/models.py
+++ services/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Service(models.Model):
     name = models.CharField(max_length=254)
+    image = models.ImageField()
     size = models.CharField(max_length=2)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
