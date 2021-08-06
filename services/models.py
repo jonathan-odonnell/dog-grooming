c7cb412255970190785c861cdf0786379b3c3975
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-06 23:00:47 +0100

    Added price model

--- services/models.py
+++ services/models.py
@@ -1,13 +1,29 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 
 class Service(models.Model):
     name = models.CharField(max_length=254)
     image = models.ImageField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    size = models.CharField(max_length=2)
     description = models.TextField()
     offer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Price(models.Model):
+    CHOICES = [
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'Extra Large'),
+    ]
+
+    service = models.ForeignKey(
+        Service, on_delete=CASCADE, related_name='prices')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    size = models.CharField(max_length=2, choices=CHOICES)
 
     def __str__(self):
         return self.name
