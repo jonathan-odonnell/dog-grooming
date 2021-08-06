5a0c565e802acef0fd5eeac7d22fa5aac3b04984
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-06 23:01:08 +0100

    Added price inline to service admin view

--- services/admin.py
+++ services/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Service, Availability
+from .models import Service, Price, Availability
 
-admin.site.register(Service)
+
+class PriceAdminInline(admin.StackedInline):
+    model = Price
+    extra = 1
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    inlines = (PriceAdminInline,)
+
+
+admin.site.register(Service, ServiceAdmin)
 admin.site.register(Availability)
