5db3c221968b2e5a787d85622673186a877e6215
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-08 21:24:48 +0100

    Added image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length=254)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
