bcdb38d3b7895ddc23179b012e134e067d350ad0
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-10 00:26:57 +0100

    Amended save method on order lineitems model

--- checkout/models.py
+++ checkout/models.py
@@ -80,7 +80,8 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.service.price * self.quantity
+        prices = self.service.prices.get(size=self.size)
+        self.lineitem_total = prices.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
