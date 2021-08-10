68a612384701c8dd41938f9e080c4be8dc298d70
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-10 16:03:45 +0100

    Added update appointments on delete to checkout signals

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,11 +1,10 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-
-from .models import OrderLineItem
+from .models import OrderLineItem, Order
 
 
 @receiver(post_save, sender=OrderLineItem)
-def update_on_save(sender, instance, created, **kwargs):
+def update_totals_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
     """
@@ -13,8 +12,16 @@ def update_on_save(sender, instance, created, **kwargs):
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_delete(sender, instance, **kwargs):
+def update_totals_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
     instance.order.update_total()
+
+
+@receiver(post_delete, sender=Order)
+def update_appointments_on_delete(sender, instance, **kwargs):
+    """
+    Update appointment on order delete
+    """
+    instance.appointments.update(reserved=False, confirmed=False)
