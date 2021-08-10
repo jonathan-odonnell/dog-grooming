51bb47839e0829cabdacced3e9f7a677cc3db526
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-10 16:20:57 +0100

    Added post request logic to home view

--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.views.generic.base import TemplateView
+from django.shortcuts import HttpResponse
+from .models import NewsletterEmail
 
 
 class HomeView(TemplateView):
     template_name = 'home/index.html'
+
+    def post(self, request):
+        NewsletterEmail.objects.create(email=request.POST['newsletter'])
+        return HttpResponse(status=200)
+
