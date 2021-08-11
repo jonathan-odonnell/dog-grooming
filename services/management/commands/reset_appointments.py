edd4fd037e939c96e43b7068dab8c81285400db4
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-11 16:51:37 +0100

    Amended reset appointments management command

--- services/management/commands/reset_appointments.py
+++ services/management/commands/reset_appointments.py
@@ -1,15 +1,16 @@
 from django.core.management.base import BaseCommand
 from services.models import Appointment
 from django.utils.timezone import now, localtime
+from datetime import timedelta
 
 
 class Command(BaseCommand):
     help = 'Resets expired reserved appointments'
 
     def handle(self, *args, **options):
-        current_time = localtime(now())
+        removal_time = localtime(now()) - timedelta(days=1)
         appointments = Appointment.objects.filter(
-            last_updated__gte=current_time).update(
+            last_updated__lte=removal_time).update(
             reserved=False, last_updated=None)
         self.stdout.write(self.style.SUCCESS(
             f'Successfully updated {appointments} appointments'))
