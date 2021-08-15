88e5770c155ee16186ac569ccd22fe94f38732d6
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-15 21:45:57 +0100

    Added pets views and urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.ProfileView.as_view(), name='profile'),
     path('orders/', views.OrdersView.as_view(), name='orders'),
     path('orders/<str:order_number>', views.OrderDetailsView.as_view(), name='order_details'),
+    path('pets/', views.PetsView.as_view(), name='pets'),
+    path('pets/add/', views.AddPetView.as_view(), name='add_pet'),
+    path('pets/edit/<int:pk>/', views.EditPetView.as_view(), name='edit_pet'),
 ]
