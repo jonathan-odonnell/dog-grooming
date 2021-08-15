9d427cacb0b5902adc4814df0a315e41b7e0664b
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-15 14:40:28 +0100

    Added autofield settings to remove warning

--- dog_grooming/settings.py
+++ dog_grooming/settings.py
@@ -138,6 +138,8 @@ else:
         }
     }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
