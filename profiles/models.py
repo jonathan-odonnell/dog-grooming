b0d82ffee6f0b9489b609f170a8148186b3d677a
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-15 20:56:35 +0100

    Added pet and breed models

--- profiles/models.py
+++ profiles/models.py
@@ -24,6 +24,30 @@ class UserProfile(models.Model):
         return self.user.username
 
 
+class Breed(models.Model):
+    CHOICES = [
+        ('Small', 'Small'),
+        ('Medium', 'Medium'),
+        ('Large', 'Large'),
+        ('Extra Large', 'Extra Large'),
+    ]
+
+    name = models.CharField(max_length=20)
+    dog_size = models.CharField(max_length=20, choices=CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=20)
+    breed = models.ForeignKey(
+        Breed, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
