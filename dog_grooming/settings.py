29bc4ce99a9aa2507499b9417e34fbb4cbdc6ab2
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-16 16:07:21 +0100

    Amended email settings

--- dog_grooming/settings.py
+++ dog_grooming/settings.py
@@ -205,18 +205,18 @@ GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', '')
 
 # Email
 
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutique@example.com'
 
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp-mail.outlook.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_HOST_USER = 'apikey'
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
 # SMS
