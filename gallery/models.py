e87d96e1b1687d2fe5e71a9af87fc34cc52424e1
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-20 16:10:57 +0100

    Added ordering to gallery model

--- gallery/models.py
+++ gallery/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Image(models.Model):
+    class Meta:
+        ordering = ('id',)
+
     name = models.CharField(max_length=254)
     image = models.ImageField()
 
