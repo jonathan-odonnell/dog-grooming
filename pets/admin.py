35b1b45633d23212c425bef078bb032d620ff385
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-20 14:24:59 +0100

    Amended breed admin view

--- pets/admin.py
+++ pets/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Pet, Breed
 
-admin.site.register(Breed)
+
+class BreedAdmin(admin.ModelAdmin):
+    ordering = ('id',)
+    list_display = ('name', 'dog_size',)
+    list_filter = ('dog_size',)
+    list_per_page = 20
+
+
+admin.site.register(Breed, BreedAdmin)
 admin.site.register(Pet)
