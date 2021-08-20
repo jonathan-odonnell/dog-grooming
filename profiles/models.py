b23d7bd21b59bbcedb76d6f7efd1bbd1320f527b
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-20 16:47:53 +0100

    Added default values to user profile mandatory fields

--- profiles/models.py
+++ profiles/models.py
@@ -12,13 +12,13 @@ class UserProfile(models.Model):
     billing information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = PhoneNumberField(max_length=20)
-    address_line_1 = models.CharField(max_length=80)
+    phone_number = PhoneNumberField(max_length=20, default='')
+    address_line_1 = models.CharField(max_length=80, default='')
     address_line_2 = models.CharField(max_length=80, null=True, blank=True)
-    town_or_city = models.CharField(max_length=40)
-    county = models.CharField(max_length=80)
-    postcode = models.CharField(max_length=20)
-    country = CountryField(blank_label='Country *')
+    town_or_city = models.CharField(max_length=40, default='')
+    county = models.CharField(max_length=80, default='')
+    postcode = models.CharField(max_length=20, default='')
+    country = CountryField(blank_label='Country *', default='')
 
     def __str__(self):
         return self.user.username
