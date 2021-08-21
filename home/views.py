e0c2553a144b7a20773b6a9decaec2c2d34f1024
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-21 16:04:32 +0100

    Added home view post request logic to add the email address to the sendgrid mailing list

--- home/views.py
+++ home/views.py
@@ -1,4 +1,19 @@
+from django.http.response import HttpResponse
 from django.views.generic.base import TemplateView
+from django.conf import settings
+import requests
+import json
+
 
 class HomeView(TemplateView):
     template_name = 'home/index.html'
+
+    def post(self, request):
+        url = 'https://api.sendgrid.com/v3/marketing/contacts'
+        payload = json.dumps({'contacts': [{'email': request.POST['email']}]})
+        headers = {
+            'authorization': f'Bearer {settings.EMAIL_HOST_PASSWORD}',
+            'content-type': 'application/json'
+        }
+        response = requests.request('PUT', url, data=payload, headers=headers)
+        return HttpResponse(response)
