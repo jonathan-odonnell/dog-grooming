a35ae3911dbc43daabc699be5edb1ddde232efdb
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-22 13:44:28 +0100

    Amended delete image url

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.GalleryView.as_view(), name='gallery'),
-    path('', views.DeleteImageView.as_view(), name='delete-image'),
+    path('delete/<int:pk>/', views.DeleteImageView.as_view(), name='delete_image'),
 ]
