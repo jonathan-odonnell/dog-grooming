91210e7daa74998a9ad6d709601ed8489e658cd0
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-25 14:46:10 +0100

    Added filter orders url

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.OrdersView.as_view(), name='orders'),
-    path('<str:order_number>/', views.OrderDetailsView.as_view(), name='order_details'),
+    path('filter/', views.OrdersView.as_view(), name='filter_orders'),
+    path('<str:order_number>/',
+         views.OrderDetailsView.as_view(), name='order_details'),
 ]
