a90dbb7fd4fa5212a306a2d69957c3c9353bffe6
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-31 16:31:59 +0100

    Amended pet form

--- pets/forms.py
+++ pets/forms.py
@@ -18,13 +18,19 @@ class PetForm(forms.ModelForm):
         self.helper.field_class = 'mb-3'
         placeholders = {
             'name': 'Name',
+            'date_of_birth': 'Date of Birth',
+            'colour': 'Colour'
         }
 
         for field in self.fields:
-            if field != 'breed':
-                placeholder = f'{placeholders[field]} *'
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            else:
+            if field == 'breed':
                 self.fields[field].empty_label = 'Breed *'
                 self.fields[field].widget.attrs['class'] = 'form-select'
+            elif field == 'gender':
+                self.fields['gender'].choices = [
+                    ('', 'Gender *'), ] + self.fields[field].choices[1:]
+                self.fields[field].widget.attrs['class'] = 'form-select'
+            else:
+                placeholder = f'{placeholders[field]} *'
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].label = False
