cd624d0f623c8ed26a28f182f78c3731e4ece1a8
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-08-31 16:19:48 +0100

    Amended pet model

--- pets/models.py
+++ pets/models.py
@@ -6,7 +6,7 @@ class Breed(models.Model):
     class Meta:
         ordering = ('id',)
 
-    CHOICES = [
+    sizes = [
         ('Small', 'Small'),
         ('Medium', 'Medium'),
         ('Large', 'Large'),
@@ -14,7 +14,7 @@ class Breed(models.Model):
     ]
 
     name = models.CharField(max_length=50)
-    size = models.CharField(max_length=20, choices=CHOICES)
+    size = models.CharField(max_length=20, choices=sizes)
 
     def __str__(self):
         return self.name
@@ -27,10 +27,18 @@ class Pet(models.Model):
     class Meta:
         ordering = ('id',)
 
+    genders = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    ]
+
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
     breed = models.ForeignKey(
         Breed, null=True, blank=True, on_delete=models.SET_NULL)
+    gender = models.CharField(max_length=10, choices=genders)
+    colour = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
