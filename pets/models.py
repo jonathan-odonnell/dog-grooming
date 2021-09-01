12e967f3d8777e93c73fb25548ef67039c4c6bd9
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-09-01 15:03:55 +0100

    Amended pets model and added vet and emergency contact models

--- pets/models.py
+++ pets/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django_countries.fields import CountryField
 from profiles.models import UserProfile
 
 
@@ -39,6 +41,50 @@ class Pet(models.Model):
         Breed, null=True, blank=True, on_delete=models.SET_NULL)
     gender = models.CharField(max_length=10, choices=genders)
     colour = models.CharField(max_length=50)
+    health = models.TextField(null=True, blank=True)
+    behaviour = models.TextField(null=True, blank=True)
+    medical_conditions = models.TextField(null=True, blank=True)
+    grooming_details = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Vet(models.Model):
+    class Meta:
+        ordering = ('id',)
+        verbose_name_plural = 'Vet'
+
+    pet = models.OneToOneField(Pet, on_delete=models.CASCADE,
+                            related_name='vet')
+    full_name = models.CharField(max_length=50)
+    phone_number = PhoneNumberField(max_length=20)
+    address_line_1 = models.CharField(max_length=80)
+    address_line_2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40)
+    county = models.CharField(max_length=80)
+    postcode = models.CharField(max_length=20)
+    country = CountryField(blank_label='Country *')
+
+    def __str__(self):
+        return self.full_name
+
+
+class EmergencyContact(models.Model):
+    class Meta:
+        ordering = ('id',)
+
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE,
+                            related_name='emergency_contact')
+    full_name = models.CharField(max_length=50)
+    relationship = models.CharField(max_length=20)
+    phone_number = PhoneNumberField(max_length=20)
+    address_line_1 = models.CharField(max_length=80)
+    address_line_2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40)
+    county = models.CharField(max_length=80)
+    postcode = models.CharField(max_length=20)
+    country = CountryField(blank_label='Country *')
+
+    def __str__(self):
+        return self.full_name
